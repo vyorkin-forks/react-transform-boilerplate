46c792e0dadbcfb0627245386dc0e8fbca4d1892
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2015-11-10 18:44:12 +0300

    up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { NICE, SUPER_NICE } from './colors';
 
+import 'react-toolbox/lib/commons';
+import Button from 'react-toolbox/lib/button';
+
 class Counter extends Component {
   constructor(props) {
     super(props);
@@ -20,9 +23,19 @@ class Counter extends Component {
 
   render() {
     return (
-      <h1 style={{ color: this.props.color }}>
-        Counter ({this.props.increment}): {this.state.counter}
-      </h1>
+      <div>
+        <h1>lets display some buttons here</h1>
+        <div>
+          <Button label="Flat button" />
+          <Button kind="raised" label="Raised" />
+          <Button kind="raised" label="Raised accent" accent icon="favorite" />
+          <Button className="primary" kind="floating" icon="add" />
+          <Button mini kind="floating" icon="add" accent />
+        </div>
+        <h1 style={{ color: this.props.color }}>
+          Counter ({this.props.increment}): {this.state.counter}
+        </h1>
+      </div>
     );
   }
 }
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,12 +1,18 @@
 var path = require('path');
 var webpack = require('webpack');
 
+var NODE_MODULES_PATH = path.resolve(__dirname, './node_modules');
+
 module.exports = {
   devtool: 'eval',
   entry: [
     'webpack-hot-middleware/client',
     './src/index'
   ],
+  resolve: {
+    root: path.resolve(__dirname, '.'),
+    extensions: ['', '.scss', '.jsx', '.js', '.json']
+  },
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
@@ -17,10 +23,24 @@ module.exports = {
     new webpack.NoErrorsPlugin()
   ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src')
-    }]
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css?modules']
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css?modules', 'sass?includePath[]=' + NODE_MODULES_PATH],
+      },
+      {
+        test: /\.sass$/,
+        loaders: ['style', 'css?modules', 'sass?includePath[]=' + NODE_MODULES_PATH + '&indentedSyntax'],
+      }
+    ]
   }
 };
